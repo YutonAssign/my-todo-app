6c58ca1ec6f1bf418cb204051beda21515f66ce5
YutonAssign <y_nakano@a-ssign.com>
2025-06-19 10:18:20 +0900

    todoアプリ作成2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "TODOアプリ",
+  title: "リマインダー",
   description: "シンプルで使いやすいTODOアプリ。タスクの管理と整理を簡単に。", // アプリの機能を説明
   keywords: ["TODO", "リマインダー", "タスク管理", "メモ"], //SEO対応
   viewport: "width=device-width, initial-scale=1", // モバイル対応の設定を明示的に追加
